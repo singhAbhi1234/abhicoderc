96e65fbfc0d918ca6d57c21431fedb6d1477b766
singhAbhi1234 <144318926+singhAbhi1234@users.noreply.github.com>
2023-09-07 01:29:41 -0600

    Add files via upload

---
+++ areaOfCircle.cpp
@@ -0,0 +1,11 @@
+//write a program to calculate area of circle
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+	double r;
+	printf("Enter the radius of the circle\n");
+	scanf("%lf",&r);
+	printf("the area of circle is %lf",3.14*r*r);
+
+}
