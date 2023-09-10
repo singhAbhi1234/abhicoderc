bd5433d90bae1fbb901500707dc313fde40c2992
singhAbhi1234 <144318926+singhAbhi1234@users.noreply.github.com>
2023-09-09 21:09:42 -0600

    Add files via upload

---
+++ calculateAverage.cpp
@@ -0,0 +1,23 @@
+//write a program to calculate average of three numbers
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+	double a,b,c;
+	double avg;
+	printf("Enter three numbers\n");
+	scanf("%lf%lf%lf",&a,&b,&c);
+	avg=(a+b+c)/3;
+	printf("the average of %lf,%lf & %lf is %lf",a,b,c,avg);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
