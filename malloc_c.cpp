575fb2950d539428866dbaaf2e9f4b490e679b10
singhAbhi1234 <144318926+singhAbhi1234@users.noreply.github.com>
2023-09-13 05:22:50 -0600

    Add files via upload

---
+++ malloc_c.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+main()
+{
+	int n,i;
+	printf("enter the size of memory\n");
+	
+	scanf("%d",&n);
+	int *ptr;
+	 ptr=(int*) malloc(n);
+	 if(ptr==NULL){
+	 	printf("memory is not created");
+	 }
+	 	printf("memory is successful created\n");
+	 for(i=0;i<5;i++){
+	 	ptr[i]=i+1;
+	 }
+	 for(i=0;i<5;i++){
+	 	printf("%d ",ptr[i]);
+}
+}
